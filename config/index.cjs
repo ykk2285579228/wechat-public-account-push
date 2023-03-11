c49ce2a5bb840dc22e5b737fc22715c48255921e
ykk2285579228 <127574791+ykk2285579228@users.noreply.github.com>
2023-03-11 11:53:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,36 +15,38 @@ const USER_CONFIG = {
 
   PROVINCE: '湖北',
   CITY: '鄂州',
+  
+  SWITCH: { 
+     //天气
+   weather: true,
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: true,
+  },
 
   USERS: [
-    {
+    { // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+       horoscopeDateType: '今日',
       // 想要发送的人的名字
       name: '小鸣',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ofurH5nOct3pMkjUJlnyhpaTpjQk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '08-26',
+      useTemplateId: 'wCIlyOjMojMwaOnfnR6AzL98es81P5Xl6JBTrPcZH2M',
+     
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1998', date: '08-26',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小小鸣', year: '2000', date: '02-19',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '03-07',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2023-03-11' },
+        
       ],
     },
   ],
